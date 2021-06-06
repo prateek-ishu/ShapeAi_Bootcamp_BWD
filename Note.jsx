b9d2c27acc071899b38e0b15950032d33ae67d9f
prateek-ishu <68288533+prateek-ishu@users.noreply.github.com>
2021-06-06 22:52:25 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This is the wonderfull bootcamp taken by Shaurya Sinha. We learn lot about react js and javascript. Project made by Prateek Dubey.
+        </p>
+      </div>
+  );
+}
+export default Note;
